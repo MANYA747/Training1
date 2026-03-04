722aa34128ebea84c97f1b4caab2b7bdffe44d55
manyahs017-prog <manyahs017@gmail.com>
2026-03-04 14:48:35 +0530

    update commit

---
+++ src/DAY6/libdemo.java
@@ -0,0 +1,26 @@
+package DAY6;
+public class libdemo {
+    static class GC extends Exception{
+        GC(String message){
+            super(message);
+        }
+    }
+    static void checkattendence(boolean absent)throws GC{
+        if(absent) throw new GC("penalty for absence");
+        else{
+            System.out.println("good students");
+        }=
+
+    }
+    public static void main(String[] args) {
+        try{
+            checkattendence(true);
+        }
+        catch(GC e){
+            System.out.println("caught an exception:"+e.getMessage());
+        }
+        finally{
+            System.out.println("don't join SNPSU");
+        }
+    }
+}
